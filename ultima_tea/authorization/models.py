d0fe44513f7ca7f609be1fe0427693bd2533196a
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2022-01-09 02:24:28 +0100

    in is int in machine

--- ultima_tea/authorization/models.py
+++ ultima_tea/authorization/models.py
@@ -51,7 +51,7 @@ class Machine(models.Model):
         OFF = 0
         ON = 1
 
-    machine_id = models.CharField(primary_key=True, max_length=32)
+    machine_id = models.CharIntegerFieldField(primary_key=True)
     brewing_temperature = models.FloatField(default=0, null=True)
     air_temperature = models.FloatField(default=0, null=True)
     mug_temperature = models.FloatField(default=0, null=True)
